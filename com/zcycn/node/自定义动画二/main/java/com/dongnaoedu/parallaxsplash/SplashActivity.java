63b0d288cfdfc490a7a152c3d896e2fbd12b3b16
zcycn <363660421@qq.com>
2017-11-18 17:02:54 +0800

    动画二

---
+++ com/zcycn/node/自定义动画二/main/java/com/dongnaoedu/parallaxsplash/SplashActivity.java
@@ -0,0 +1,33 @@
+package com.dongnaoedu.parallaxsplash;
+
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.widget.ImageView;
+
+public class SplashActivity extends FragmentActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+		
+		ParallaxContainer container = (ParallaxContainer) findViewById(R.id.parallax_container);
+		container.setUp(new int[]{
+			R.layout.view_intro_1,
+			R.layout.view_intro_2,
+			R.layout.view_intro_3,
+			R.layout.view_intro_4,
+			R.layout.view_intro_5,
+			R.layout.view_login
+		});
+		
+		//设置动画
+		ImageView iv_man = (ImageView) findViewById(R.id.iv_man);
+		iv_man.setBackgroundResource(R.anim.man_run);
+		container.setIv_man(iv_man);
+		
+	}
+
+
+}
